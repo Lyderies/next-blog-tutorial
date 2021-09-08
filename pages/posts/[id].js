217ebb82e8b1dd82773e5b2ba17d083dea283f98
Lyderies <lyderies@hotmail.com>
2021-09-08 14:04:29 +1200

    I broke it here

---
+++ pages/posts/[id].js
@@ -0,0 +1,38 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
+
+export async function getStaticProps({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths({ params }) {
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id);
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      {postData.title}
+      <br />
+      {postData.id}
+      <br />
+      {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+    </Layout>
+  );
+}
